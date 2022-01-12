a1cd5f48a77bbcbeadcd16651ba267b210ad8c30
jesica <jesicaguerrerof@gmail.com>
2022-01-11 23:00:24 -0300

    tp done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './app.css';
+import Sidebar from './components/SideBar';
+import ContentWrapper from './components/ContentWrapper';
+
+
+function App() {
+  return (
+    <div id="wrapper">
+
+		<Sidebar/>
+
+		<ContentWrapper/>
+
+	</div>
+  );
+}
+
+export default App;
